7d19246d515c35c6793f1dd7d7bd000d27083171
lasitha <mbel002@mbel.co.jp>
2020-10-09 17:15:04 +0900

    Maintenence API

--- app/Http/Controllers/API/MaintenanceController.php
+++ app/Http/Controllers/API/MaintenanceController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MaintenanceResource;
+use App\Http\Resources\MaintenanceResources;
+use App\Http\Resources\MemoResources;
 use App\Models\Maintenance;
+use App\Models\Memo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MaintenanceController extends Controller
 {
@@ -12,19 +17,10 @@ class MaintenanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $perPage = $request->query('perPage') ? (int)$request->query('perPage') : 15;
+        return new MaintenanceResources(Maintenance::with('user', 'images')->paginate($perPage));
     }
 
     /**
@@ -35,7 +31,28 @@ class MaintenanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validating
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'nullable|max:255',
+            'lat' => 'nullable|max:30',
+            'lng' => 'nullable|max:30',
+            'device_id' => 'required|exists:App\Models\Device,id',
+            'imageUrls.*' => 'url'
+        ]);
+
+        $maintenance = new Maintenance($validatedData);
+        $maintenance->user_id = Auth::user()->id;
+        $maintenance->save();
+
+        $urls = [];
+        foreach ($request->url as $url) {
+            $urls[] = ['url' => $url];
+        }
+        $maintenance->images()->createMany($urls);
+
+        return response($maintenance->load('images'), 201);
+
     }
 
     /**
@@ -46,7 +63,9 @@ class MaintenanceController extends Controller
      */
     public function show(Maintenance $maintenance)
     {
-        //
+        $maintenance = new MaintenanceResource($maintenance->load(['user', 'device', 'images']));
+
+        return $maintenance;
     }
 
     /**
@@ -55,22 +74,34 @@ class MaintenanceController extends Controller
      * @param  \App\Models\Maintenance  $maintenance
      * @return \Illuminate\Http\Response
      */
-    public function edit(Maintenance $maintenance)
+    public function edit(Maintenance $maintenance, Request $request)
     {
-        //
+        //validating
+        $validatedData = $request->validate([
+            'name' => 'max:255',
+            'description' => 'nullable|max:255',
+            'lat' => 'nullable|max:30',
+            'lng' => 'nullable|max:30',
+            'device_id' => 'exists:App\Models\Device,id',
+            'imageUrls.*' => 'url'
+        ]);
+
+        $maintenance->update($request->all());
+
+        // Update image list
+        if ($request->url) {
+            $maintenance->images()->delete();
+
+            $urls = [];
+            foreach ($request->url as $url) {
+                $urls[] = ['url' => $url];
+            }
+            $maintenance->images()->createMany($urls);
+        }
+
+        return response($maintenance);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Maintenance  $maintenance
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Maintenance $maintenance)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -80,6 +111,18 @@ class MaintenanceController extends Controller
      */
     public function destroy(Maintenance $maintenance)
     {
-        //
+        $maintenance->images()->delete();
+        $maintenance->delete();
+        return response(['message' => 'Success!'], 200);
     }
+
+    /**
+     * Get memos of a Maintenance
+     */
+
+     public function getMemos($maintenance, Request $request)
+     {
+        $perPage = $request->query('perPage') ? (int)$request->query('perPage') : 15;
+        return new MemoResources(Memo::where('maintenance_id',$maintenance)->with(['user','images'])->paginate($perPage));
+     }
 }
