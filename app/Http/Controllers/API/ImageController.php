5a948d459f3bbd7688d8123ae535683c3679aece
Mithunraj <mbel001@mbel.co.jp>
2020-10-16 15:20:23 +0900

    image controller

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -8,6 +8,38 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *      path="/images",
+     *      tags={"Image"},
+     *      summary="Store new image",
+     *   security={ {"bearer": {} }},
+     *      description="Returns images data",
+     *    @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="image",
+     *                      description="Item image",
+     *                      type="array",
+     *                      @OA\Items(type="string", format="binary")
+     *                   ),
+     *               ),
+     *           ),
+     *       ),
+     *
+     *      @OA\Response(response=200,description="Image Url received",
+     *          @OA\MediaType(mediaType="application/json")
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Access denied!"
+     *      )
+     * )
+     */
     public function store(Request $request)
     {
         //validating
@@ -16,9 +48,9 @@ class ImageController extends Controller
         ]);
 
         // Start uploading s3
-        $image  =  $request-> file('image');
-        $path  =  Storage::disk( 's3' )->put('images' ,  $image ,  'public');
+        $image  =  $request->file('image');
+        $path  =  Storage::disk('s3')->put('images',  $image,  'public');
 
-        return response(['imageUrl'=>$path,'uri'=>env('AWS_S3_URL').$path,'prefix'=>env('AWS_S3_URL')], 201);
+        return response(['imageUrl' => $path, 'uri' => env('AWS_S3_URL') . $path, 'prefix' => env('AWS_S3_URL')], 201);
     }
 }
