1368800cedf8a37f90a6acdef9e194d7e7a5c49b
Mithunraj <mbel001@mbel.co.jp>
2020-11-06 14:15:21 +0900

    composer warnings fixed

--- app/Http/Controllers/API/MaintenanceController.php
+++ app/Http/Controllers/API/MaintenanceController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\MaintenanceResource;
 use App\Http\Resources\MaintenanceResources;
 use App\Http\Resources\MemoResources;
